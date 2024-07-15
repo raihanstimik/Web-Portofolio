f4293706cfe27637364e6075054d6871c8c7dfd8
raihanstimik <m.raihan.h.p14@gmail.com>
2024-07-16 04:28:39 +0700

    finishing backend#2

--- resources/views/admin/data_table/index.blade.php
+++ resources/views/admin/data_table/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 @section('admin')
-@vite(['resources/css/app.css', 'resources/js/app.js'])
+@vite(['resources/sass/app.scss', 'resources/js/app.js'])
+@vite('resources/css/app.css')
 
-
+<p>haloooooooo</p>
 @endsection
