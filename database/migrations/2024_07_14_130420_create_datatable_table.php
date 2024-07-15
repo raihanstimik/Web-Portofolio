8c32792672d9696306ffa9893879dfa4b74cac0d
mahardikaayu17 <mahardikaayuwida@gmail.com>
2024-07-15 17:49:34 +0700

    edit readme (#3)
    * edit readme

--- database/migrations/2024_07_14_130420_create_datatable_table.php
+++ database/migrations/2024_07_14_130420_create_datatable_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('datatable', function (Blueprint $table) {
             $table->id();
-            $table->string('');
             $table->timestamps();
         });
     }
